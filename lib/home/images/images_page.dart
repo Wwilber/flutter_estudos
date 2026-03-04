7358cb5ced23be9368495484398e031c9a218f6c
xxx <93487277+Wwilber@users.noreply.github.com>
2026-03-04 16:55:10 -0300

    estudos images

--- lib/home/images/images_page.dart
+++ lib/home/images/images_page.dart
@@ -10,7 +10,32 @@ class ImagesPage extends StatelessWidget {
         title: const Text('Images Page', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.blue,
       ),
-      body: Center(child: Image.asset('assets/images/Paisagem_no_inhotim.jpg')),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
+              child: Image.asset(
+                'assets/images/Paisagem_no_inhotim.jpg',
+                fit: BoxFit.cover,
+                // fit: BoxFit.contain,
+              ),
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              // color: Colors.red,
+              child: Image.network(
+                'https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png',
+                fit: BoxFit.cover,
+                // fit: BoxFit.contain,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
